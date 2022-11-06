675596e92ae5e1b4d09be9e0a068b995a6ee0617
M Newman <megan.m.boggs@gmail.com>
2022-11-05 22:35:10 -0700

    add append option

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads);
 });
 
+
+// another way to do this same thing:
+// // create element
+    // set text content
+    // append to ul
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+
 for (let i = 0; i < myLeads.length; i++) {
   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 }
